80f34ef73e42106cb028fca1d79c8a4aa1637ee1
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-04-22 21:25:38 +0400

    add loop

--- src/sumDigits.java
+++ src/sumDigits.java
@@ -6,7 +6,9 @@ public class sumDigits {
     public static Integer sumDigits(String str){
         int count = 0;
         for (int i = 0 ; i<str.length();i++){
-            if (str.charAt(i))
+            if (Character.isDigit(str.charAt(i)){
+                count++;
+            }
         }
 
     }
