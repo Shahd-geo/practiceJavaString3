79d857cff35f54f8f45542b1da8fe0cb88ce26cc
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-04-22 21:11:44 +0400

    return count

--- src/task5.java
+++ src/task5.java
@@ -7,9 +7,10 @@ public class task5 {
       int count3=0;
       for (int i =0;i<str.length()-1;i++){
           if (str.charAt(i)==str.charAt(i+1)&&str.charAt(i)==str.charAt(i+2)){
-
+                count3++;
           }
       }
+      return count3;
 
     }
 
