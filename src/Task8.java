2972f0b886675004d638ed6d1456f3ed740b371e
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-04-22 21:55:20 +0400

    mirrorEnds

--- src/Task8.java
+++ src/Task8.java
@@ -1,12 +1,13 @@
 public class Task8 {
     static void main(String[] args) {
-        String str="abXYZba"
+        String str="abXYZba";
+        System.out.println(mirrorEnds(str));
 
     }
     public static String mirrorEnds(String str) {
         String result = "";
         for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == str.charAt(str.length() - 1)) {
+            if (str.charAt(i) == str.charAt(str.length() - 1-i)) {
                 result += str.charAt(i);
             } else {
                 break;
