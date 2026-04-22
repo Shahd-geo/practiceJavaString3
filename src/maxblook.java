09657c591849217e4d4c22f1f1397aa525675f0b
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-04-22 22:06:20 +0400

    return max

--- src/maxblook.java
+++ src/maxblook.java
@@ -2,19 +2,24 @@ public class maxblook {
     static void main(String[] args) {
 
     }
-    public static Integer max(String str){
-        int block=1;
-        int max =0;
-        for (int i =0 ;i<str.length();i++){
-            if (i<str.length()-1 &&str.charAt(i)==str.charAt(i+1)){
+
+    public static Integer max(String str) {
+        int block = 1;
+        int max = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)) {
                 block++;
-            }else {
-                if (block<max){
-                    max=block;
+            } else {
+                if (block < max) {
+                    max = block;
                 }
-                block=1;
+                block = 1;
             }
+
         }
+        return max;
+    }
+}
 
     }
 }
