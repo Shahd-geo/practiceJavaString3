ec51bf9c8262b7543418a8e59a3136b297b7bcd7
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-04-22 21:04:29 +0400

    solve task4

--- src/Task4.java
+++ src/Task4.java
@@ -1,6 +1,6 @@
 public class Task4 {
     static void main(String[] args) {
-        String str="xxggyygxx";
+        String str = "xxggxx";
         System.out.println(gHappy(str));
 
 
@@ -8,15 +8,18 @@ public class Task4 {
 
     public static Boolean gHappy(String str) {
         for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == 'g' && str.charAt(i - 1) == 'g' || str.charAt(i + 1) == 'g') {
-                return true;
-            } else {
-                return false;
+            if (str.charAt(i) == 'g') {
+                if ((i > 0 && str.charAt(i - 1) == 'g') || i > str
+                        .length() - 1 && str.charAt(i + 1) == 'g') {
+                    return true;
+                } else {
+                    return false;
+                }
+
             }
 
         }
         return true;
     }
-
 }
 
