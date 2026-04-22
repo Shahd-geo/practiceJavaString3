031534b73ee90d7417bda62cf01963537f548637
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-04-22 21:10:57 +0400

    add loop

--- src/task5.java
+++ src/task5.java
@@ -5,6 +5,11 @@ public class task5 {
 
     public static Integer countTriple(String str){
       int count3=0;
+      for (int i =0;i<str.length()-1;i++){
+          if (str.charAt(i)==str.charAt(i+1)&&str.charAt(i)==str.charAt(i+2)){
+
+          }
+      }
 
     }
 
