422e500dd52b2b1029ad142f4795d77b84b82014
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-04-22 21:38:27 +0400

    add end str var

--- src/sameEnd.java
+++ src/sameEnd.java
@@ -7,6 +7,7 @@ public class sameEnd {
         String result="";
         for (int i = 0 ; i>=str.length()/2;i++){
             String start=str.substring(0,i);
+            String end = str.substring(leng-i);
 
         }
 
