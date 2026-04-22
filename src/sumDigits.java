05d814a3d2f156b18ab24b0e47392b13249f9699
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-04-22 21:28:51 +0400

    sum digit

--- src/sumDigits.java
+++ src/sumDigits.java
@@ -7,13 +7,15 @@ public class sumDigits {
 
     public static Integer sumDigits(String str){
         int count = 0;
+        int sum=0;
         for (int i = 0 ; i<str.length();i++){
-            if (Character.isDigit(str.charAt(i)){
+            if (Character.isDigit(str.charAt(i))){
                 count++;
+                sum+=count;
             }
         }
 
-        return count;
+        return sum;
 
     }
 
