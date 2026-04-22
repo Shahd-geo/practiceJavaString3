e771d35b70e820271fa2001c726cae33eef464c3
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-04-22 20:51:38 +0400

    add gHappy class

---
+++ src/Task4.java
@@ -0,0 +1,11 @@
+public class Task4 {
+    static void main(String[] args) {
+
+    }
+    public static Boolean gHappy(String str){
+        for (int i = 0 ; i<str.length();i++){
+            if (str.ch)
+        }
+
+    }
+}
